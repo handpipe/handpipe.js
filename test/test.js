98e394987ffac17c9c43907c78cf62b6b5fb31a0
Alan Shaw <alan@freestyle-developments.co.uk>
2014-06-25 10:16:33 +0100

    Proper tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,35 @@
 var fs = require("fs")
-var genplate = require("../")
+  , test = require("tape")
+  , concat = require("concat-stream")
+  , genplate = require("../")
 
-fs.createReadStream(__dirname + "/fixtures/template.html")
-  .pipe(genplate({
-    title: function (next, cb) {
-      setTimeout(function () {
-        cb(null, "Foo")
-      }, 2000)
-    },
-    tweets: [
-      {text: "Oh HAI!", hashtags: ["OMG", "WTF"], author: "Foo Bar"},
-      {text: "OMGWTF!"}
-    ],
-    author: function (next, cb) {
-      cb(null, "Author" + next.index)
-    }
-  }))
-  .pipe(process.stdout)
+var templatesDir = __dirname + "/fixtures/templates"
+  , expectationsDir = __dirname + "/fixtures/expectations"
+
+function setupTest (template, data, cb) {
+  fs.createReadStream(templatesDir + "/" + template)
+    .pipe(genplate(data))
+    .pipe(concat({encoding: "string"}, function (actual) {
+      fs.readFile(expectationsDir + "/" + template, "utf8", function (er, expected) {
+        cb(er, {template: template, actual: actual, expected: expected})
+      })
+    }))
+}
+
+test("variable", function (t) {
+  t.plan(2)
+  setupTest("variable.html", {title: "Hello World!"}, function (er, data) {
+    t.ifError(er, "Error during " + data.template + " setup")
+    t.equal(data.actual, data.expected, "Unexpected contents " + data.template)
+  })
+})
+
+test("variable with whitespace", function (t) {
+  t.plan(2)
+  setupTest("variable-w-whitespace.html", {title: "Hello World!"}, function (er, data) {
+    t.ifError(er, "Error during " + data.template + " setup")
+    t.equal(data.actual, data.expected, "Unexpected contents " + data.template)
+  })
+})
+
+
